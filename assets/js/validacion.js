6e3e7a4a5a4bc1883e01c7f58ac911b9e95339f7
DSTAngelo <dstgps01@gmail.com>
2022-06-28 23:11:35 -0500

    Sprint_One:28062022-4

--- assets/js/validacion.js
+++ assets/js/validacion.js
@@ -1,1 +1,42 @@
-//Haz tú validación en javascript acá
+function limpiar_cajas() {
+  var nombre = document.querySelector("#nombre_input");
+  var mail = document.querySelector("#mail_input");
+  var asunto = document.querySelector("#asunto_input");
+  var mensaje = document.querySelector("#mensaje_text");
+
+  nombre.value = "";
+  mail.value = "";
+  asunto.value = "";
+  mensaje.value = "";
+}
+
+function validacion_cajas() {
+  var nombre = document.querySelector("#nombre_input");
+  var mail = document.querySelector("#mail_input");
+  var asunto = document.querySelector("#asunto_input");
+  var mensaje = document.querySelector("#mensaje_text");
+
+    nombre.value = nombre.value.trim();
+    mail.value = mail.value.trim();
+    asunto.value = asunto.value.trim();
+    mensaje.value = mensaje.value.trim();
+  if (
+    nombre.value == "" ||
+    mail.value == "" ||
+    asunto.value == "" ||
+    mensaje.value == ""
+  ) {
+    console.log("no se puede enviar caja vacia");
+    nombre.focus();
+    document.getElementById('label_contenido').innerHTML = 'Campos Obligatorios';
+    event.preventDefault();
+    return false;
+  } else {
+    console.log("datos enviados"); 
+    document.getElementById("label_contenido").innerHTML =
+      "Datos enviados correctamente";
+      limpiar_cajas();
+    event.preventDefault();
+    return true;
+  }
+}
